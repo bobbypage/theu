7a94520b9b814ba0a9d4d75503815c88d223648b
David Porter <david@porter.me>
2019-02-17 14:54:21 -0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
 class App extends Component {
@@ -7,10 +9,26 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1> The U </h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <style type="text/css">
+          {`
+            .btn-flat {
+              background-color: purple;
+              color: white;
+            }
+
+            .btn-xxl {
+              padding: 1rem 1.5rem;
+              font-size: 1.5rem;
+            }
+            `}
+          </style>
+          <Button variant="flat" size="xxl">
+            flat button
+          </Button>
           <a
             className="App-link"
             href="https://reactjs.org"
