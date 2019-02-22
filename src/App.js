63935c6ad3e1521341c3414cc8e4dba5fe69b179
aliale <aleghfeli96@gmail.com>
2019-02-21 20:04:41 -0800

    howdy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import NavFooter from './NavFooter'
-
+import NavFooter from './NavFooter';
 import AboutUs from './AboutUs';
 import FrequentlyAskedQuestions from './FrequentlyAskedQuestions';
 import CodeOfConduct from './CodeOfConduct';
@@ -11,6 +10,10 @@ import HomePage from './HomePage';
 import Forum from './Forum';
 import Post from './Post';
 import NewPost from './NewPost';
+import Login from "./Login";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 // You can choose your kind of history here (e.g. browserHistory)
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
@@ -19,7 +22,16 @@ class App extends Component {
     render() {
         return (
             <BrowserRouter>
+
+
               <div id="container">
+
+              <Container>
+                 <Row>
+                     <Login/>
+                 </Row>
+              </Container>
+
                 <div id="body">
                     <Switch>
                       <Route exact path="/" component={HomePage} />
@@ -42,4 +54,3 @@ class App extends Component {
 }
 
 export default App;
-
